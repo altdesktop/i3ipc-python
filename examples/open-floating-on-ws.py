9bdc260cb9684a5d4133187c38b953f61c4b6db5
Tony Crisci <tony@dubstepdish.com>
2014-04-18 16:10:08 -0400

    Add open-floating-on-ws example
    This example shows how to make any window that opens on a workspace
    floating

---
+++ examples/open-floating-on-ws.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# This example shows how to make any window that opens on a workspace floating
+
+# All workspaces that start with a string in this list will have their windows
+# open floating
+FLOATING_WORKSPACES = [ '3' ]
+
+def is_ws_floating(name):
+    for floating_ws in FLOATING_WORKSPACES:
+        if name.startswith(floating_ws):
+            return True
+
+    return False
+
+from gi.repository import i3ipc
+
+i3 = i3ipc.Connection()
+
+def on_window_open(i3, e):
+    ws = i3.get_tree().find_focused().workspace()
+    if is_ws_floating(ws.props.name):
+        e.container.command('floating toggle')
+
+i3.on('window::new', on_window_open)
+
+i3.main()
