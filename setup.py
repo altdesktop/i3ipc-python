9c7fb4a65b28a52131fe1ae9e5783b3b75caac3d
Tony Crisci <tony@dubstepdish.com>
2015-03-06 21:45:49 -0500

    Remove python-xlib python2 dependency
    Only depend on python3-xlib. This dependency seems to work fine for both
    python versions.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['python3-xlib', 'python-xlib', 'enum34'],
+    install_requires=['python3-xlib', 'enum34'],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
