800ee1ea94b859938e82e3d34a4cfb887733eb11
Tony Crisci <tony@dubstepdish.com>
2015-03-06 21:50:39 -0500

    Bump to version 1.1.2
    This version fixes a dependency conflict that was breaking installation
    with pip.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
-    version='1.1.1',
+    version='1.1.2',
 
     description='An improved Python library for i3wm extensions',
     long_description=long_description,
