83b1b8df2237364774c784d90eb3b8eb6fb89bdc
Anon1234 <github@forspam.mail.ua>
2015-10-24 01:15:21 +0300

    Add focus_next_visible.py example

---
+++ examples/focus_next_visible.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+focus_next_visible.py - cycles input focus between visible windows on workspace
+
+ - requires https://github.com/acrisci/i3ipc-python
+
+ Usage:
+
+    # focus next visible window
+    bindsym $mod+n exec --no-startup-id focus_next_visible.py
+
+    # focus previous visible window
+    bindsym $mod+Shift+n exec --no-startup-id focus_next_visible.py reverse
+
+
+https://faq.i3wm.org/question/6937/move-focus-from-tabbed-container-to-win...
+
+"""
+
+from sys import argv
+from itertools import cycle
+from subprocess import check_output
+
+import i3ipc
+
+
+def get_windows_on_ws(conn):
+    return filter(lambda x: x.window,
+                  conn.get_tree().find_focused().workspace().descendents())
+
+
+def find_visible_windows(windows_on_workspace):
+    visible_windows = []
+    for w in windows_on_workspace:
+        xprop = check_output(['xprop', '-id', str(w.window)]).decode()
+
+        if '_NET_WM_STATE_HIDDEN' not in xprop:
+            visible_windows.append(w)
+
+    return visible_windows
+
+
+if __name__ == '__main__':
+
+    conn = i3ipc.Connection()
+
+    visible_windows = find_visible_windows(get_windows_on_ws(conn))
+
+    if len(argv) > 1 and argv[1] == "reverse":
+        cycle_windows = cycle(reversed(visible_windows))
+    else:
+        cycle_windows = cycle(visible_windows)
+
+    for window in cycle_windows:
+        if window.focused:
+            focus_to = next(cycle_windows)
+            conn.command('[id="%d"] focus' % focus_to.window)
+            break
