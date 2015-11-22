e2b3809cf4705b0ab97288c69f0ee2680a932759
Tony Crisci <tony@dubstepdish.com>
2015-11-22 17:09:54 -0500

    Add example: workspace-new.py
    Add a simple script to go to a new workspace. It will switch to a
    workspace with the lowest available number.

---
+++ examples/workspace-new.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import i3ipc
+import re
+from argparse import ArgumentParser
+
+def main():
+    parser = ArgumentParser(description='''
+    Simple script to go to a new workspace. It will switch to a workspace with the lowest available number.
+    ''')
+    parser.parse_args()
+
+    i3 = i3ipc.Connection()
+
+    workspaces = i3.get_workspaces()
+    numbered_workspaces = filter(lambda w: w.name[0].isdigit(), workspaces)
+    numbers = list(map(lambda w: int(re.search(r'^([0-9]+)', w.name).group(0)), numbered_workspaces))
+
+    new = 0
+
+    for i in range(1, max(numbers) + 2):
+        if i not in numbers:
+            new = i
+            break
+
+    i3.command("workspace %s" % new)
+
+if __name__ == '__main__':
+    main()
