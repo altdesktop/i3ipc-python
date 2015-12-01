4bd5bafa0e69bbc6b31e55044fc22c6bc89fcf0c
Tony Crisci <tony@dubstepdish.com>
2015-11-30 22:00:01 -0500

    disable-standby-fs.py: handle multiple fs windows

--- examples/disable-standby-fs.py
+++ examples/disable-standby-fs.py
@@ -14,18 +14,27 @@ parser = ArgumentParser(prog='disable-standby-fs',
 
 args = parser.parse_args()
 
-def on_fullscreen_mode(i3, e):
-    if e.container.props.fullscreen_mode:
+def find_fullscreen(con):
+    # XXX remove me when this method is available on the con in a release
+    return [c for c in con.descendents()
+            if c.type == 'con' and c.fullscreen_mode]
+
+def set_dpms(state):
+    if state:
+        print('setting dpms on')
+        call(['xset', 's', 'on'])
+        call(['xset', '+dpms'])
+    else:
+        print('setting dpms off')
         call(['xset', 's', 'off'])
         call(['xset', '-dpms'])
-    else:
-        call(['xset', 's', 'on'])
-        call(['xset', '+dpms'])
+
+def on_fullscreen_mode(i3, e):
+    set_dpms(not len(find_fullscreen(i3.get_tree())))
 
 def on_window_close(i3, e):
-    if e.container.props.fullscreen_mode:
-        call(['xset', 's', 'on'])
-        call(['xset', '+dpms'])
+    if not len(find_fullscreen(i3.get_tree())):
+        set_dpms(True)
 
 i3.on('window::fullscreen_mode', on_fullscreen_mode)
 i3.on('window::close', on_window_close)
