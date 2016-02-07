71888e5257a657935946b43699fce08e6edf5313
Ilya Tumaykin <itumaykin@gmail.com>
2016-02-07 21:24:21 +0300

    Fix indent

--- i3ipc.py
+++ i3ipc.py
@@ -32,7 +32,7 @@ class Event(object):
 class _ReplyType(dict):
 
     def __getattr__(self, name):
-            return self[name]
+        return self[name]
 
     def __setattr__(self, name, value):
         self[name] = value
