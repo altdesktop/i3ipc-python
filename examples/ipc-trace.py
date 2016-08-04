44d5a6094a0e1e2a4f5654713802bcfe42499839
Tony Crisci <tony@dubstepdish.com>
2016-08-03 20:58:47 -0400

    add ipc-trace example

---
+++ examples/ipc-trace.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import i3ipc
+from time import strftime, gmtime
+
+i3 = i3ipc.Connection()
+
+def print_separator():
+    print('-----')
+
+def print_time():
+    print(strftime(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+
+def print_con_info(con):
+    if con:
+        print('Id: %s' % con.id)
+        print('Name: %s' % con.name)
+    else:
+        print('(none)')
+
+def on_window(i3, e):
+    print_separator()
+    print('Got window event:')
+    print_time()
+    print('Change: %s' % e.change)
+    print_con_info(e.container)
+
+def on_workspace(i3, e):
+    print_separator()
+    print('Got workspace event:')
+    print_time()
+    print('Change: %s' % e.change)
+    print('Current:')
+    print_con_info(e.current)
+    print('Old:')
+    print_con_info(e.old)
+
+
+# TODO subscribe to all events
+
+i3.on('window', on_window)
+i3.on('workspace', on_workspace)
+
+i3.main()
