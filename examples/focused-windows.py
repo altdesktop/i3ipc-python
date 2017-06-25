2197e16cf20bba5d373f4b7a250b8f1190be8ede
Nemo Nightingale <kartoffelspam@gmail.com>
2017-06-26 01:43:44 +0200

    Add focus attribute example.

---
+++ examples/focused-windows.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import i3ipc
+
+i3 = i3ipc.Connection()
+
+def focused_windows():
+  tree = i3.get_tree()
+
+  workspaces = tree.workspaces()
+  for workspace in workspaces:
+    container = workspace
+
+    while container:
+      if not hasattr(container, 'focus') \
+          or not container.focus:
+        break
+
+      container_id = container.focus[0]
+      container = container.find_by_id(container_id)
+
+    if container:
+      coname = container.name
+      wsname = workspace.name
+
+      print('WS', wsname +':', coname)
+
+
+if __name__ == '__main__':
+  parser = ArgumentParser(description = 'Print the names of the focused window of each workspace.')
+  parser.parse_args()
+
+  focused_windows()
