341eb0d9ec9f13ca27005e0a4511da227deef3ee
Tony Crisci <tony@dubstepdish.com>
2018-07-17 19:08:41 -0400

    Raise AttributeError instead of KeyError for _ReplyType class
    When getting an attribute that is not there, some people might expect to
    catch AttributeError and raise any other which leads to bugs.
    
    fixes #89

--- i3ipc/i3ipc.py
+++ i3ipc/i3ipc.py
@@ -39,7 +39,10 @@ class Event(object):
 
 class _ReplyType(dict):
     def __getattr__(self, name):
-        return self[name]
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
 
     def __setattr__(self, name, value):
         self[name] = value
