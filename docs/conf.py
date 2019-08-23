da39dc0451a3215187ee635884d67b376176ea7e
Tony Crisci <tony@dubstepdish.com>
2019-08-23 10:58:50 -0400

    docs: fix library import

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../i3ipc'))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/..'))
 
 # -- General configuration ------------------------------------------------
 
