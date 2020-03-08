0ae8cce9a43867784d4472af31a0f1145b7f0d02
Tony Crisci <tony@dubstepdish.com>
2020-03-08 17:03:52 -0400

    test: fix test_workspace flakes

--- test/aio/test_workspace.py
+++ test/aio/test_workspace.py
@@ -1,6 +1,7 @@
 from .ipctest import IpcTest
 
 import pytest
+import asyncio
 from i3ipc import Event
 
 
@@ -13,10 +14,12 @@ class TestWorkspace(IpcTest):
 
     @pytest.mark.asyncio
     async def test_workspace(self, i3):
+        await i3.command('workspace 0')
         await i3.subscribe([Event.WORKSPACE])
         i3.on(Event.WORKSPACE_INIT, self.on_workspace)
+        main_cor = i3.main()
         await i3.command('workspace 12')
-        await i3.main()
+        await main_cor
         workspaces = await i3.get_workspaces()
 
         assert len(workspaces) == 1
